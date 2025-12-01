70bfe36fbc3aa9bf5851c20034cfccbed0c343bd
Erbol Zhaparov <ae.270718@gmail.com>
2025-12-02 01:15:19 +0600

    add EmployeeRecordData class for jackson de/serializations

---
+++ src/kg/attractor/java/model/EmployeeRecordData.java
@@ -0,0 +1,19 @@
+package kg.attractor.java.model;
+
+public class EmployeeRecordData {
+    private final Employee employee;
+    private final EmployeeRecords records;
+
+    public EmployeeRecordData(Employee employee, EmployeeRecords records) {
+        this.employee = employee;
+        this.records = records;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public EmployeeRecords getRecords() {
+        return records;
+    }
+}
